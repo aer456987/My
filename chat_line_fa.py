81bbab0b3cfeda84780289654fabc598cc9484fd
Hua Yen <aer456987@gmail.com>
2020-04-17 09:15:53 +0800

    自動取代名字

--- chat_line_fa.py
+++ chat_line_fa.py
@@ -11,13 +11,13 @@ def read_file(filename):
 def field(lines):
     fields = []
     for fie in lines:
-        if '2019/' in fie[0]:  # 如果裡面含有'2019/'就忽略
+        if '2019/' in fie[0]:     # 如果裡面含有'2019/'就忽略
             continue
-        elif not str(fie[0]):  # 如果是空行就忽略
+        elif not str(fie[0]):     # 如果是空行就忽略
             continue
-        elif len(fie) > 1:  # 如果是清單禮物件>1,則從第2個物件開始加入清單
+        elif len(fie) > 1:        # 如果是清單禮物件>1,就從第2個物件開始加入清單
             fields.append(fie[1:])
-        else:  # 除了以上條件以外就直接加入清單
+        else:                     # 除了以上條件以外就直接加入清單
             fields.append(fie)
     return fields
 
@@ -27,6 +27,8 @@ def convert(lines):
     news = []
     for new in lines:
         if len(new) > 1:
+            if new[0] == 'Yuzzzzzz':
+                new[0] = 'Yuz'
             news.append(new[0] + '： ' + new[1])
         else:
             news.append(new)
@@ -45,6 +47,7 @@ def main():
     lines = read_file(filename)
     lines = field(lines)
     lines = convert(lines)
-    save('[LINE]fa_output.csv',lines)
-    save('[LINE]fa_output.txt',lines)
+    # save('[LINE]fa_output.csv',lines)
+    # save('[LINE]fa_output.txt',lines)
+    save('[LINE]fa_output.doc',lines)
 main()

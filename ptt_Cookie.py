11136caa677ba98e811fe3119b25f9ff52b883bb
Tori <aer456987@gmail.com>
2020-04-27 15:37:17 +0800

    ru8 bj4

--- ptt_Cookie.py
+++ ptt_Cookie.py
@@ -14,25 +14,33 @@ def getdata(url):
     return data
 
 
-# 資料解析
+# 資料解析,取得標題和上一頁的網頁連結
 def analyzedata(data):
     import bs4
-    root = bs4.BeautifulSoup(data, 'html.parser')   # 用BeautifulSoup4開啟上面抓到的html網頁
+    root = bs4.BeautifulSoup(data, 'html.parser')    # 用BeautifulSoup4開啟上面抓到的html網頁
 
     # 變數.find_all('標籤', 篩選條件)  → (全部)從網頁中尋找標籤
-    titles = root.find_all('div', class_='title')   # 搜尋'div'標籤中含有class_='title'的資料
+    titles = root.find_all('div', class_='title')    # 搜尋'div'標籤中含有class_='title'的資料
     for title in titles:
-        if title.a != None:      # 如果title中有a標籤就印出(排除已被刪除的文章)
+        if title.a != None:          # 如果title中有a標籤就印出(排除已被刪除的文章)
             print(title.a.string)
 
-    nextLink = root.find('a', string='‹ 上頁')      # 搜尋'a'標籤中string內文是'‹ 上頁'的資料
-    # print(nextLink['href'])  # 印出nextLink(a標籤)標籤中的'href'屬性，會印出網址
-    print('上一頁網址：https://www.ptt.cc' + nextLink['href'])
+    # 搜尋'a'標籤中string內文是'‹ 上頁'的資料
+    nextLink = root.find('a', string='‹ 上頁')
+    return nextLink['href']          # 回傳nextLink(a標籤)標籤中的'href'屬性，產生的網址會被存入
 
 
+# 執行
 def main():
     url = 'https://www.ptt.cc/bbs/Gossiping/index.html'
-    data = getdata(url)
-    analyzedata(data)
+    count = 0
+    while count < 3:
+        count += 1
+        print('=================================\n' +
+            '【第', count, '頁】\n' +
+            '================================='
+        )
+        data = getdata(url)
+        data = 'https://www.ptt.cc' + analyzedata(data)
 
 main()
